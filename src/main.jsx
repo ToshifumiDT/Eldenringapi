93251d2aae94ac633c4c1e4569fa172c75881146
ToshifumiDT <toshifumi.digitaletalenter@gmail.com>
2024-08-28 15:18:21 +0200

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-
-
-
+ReactDOM.render(
+  <React.StrictMode>
+    <Router basename="/Eldenringapi">
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
